a7181c97120a975e078378598c655cbd5f960866
Tomasz Tomczyk <me@tomasztomczyk.com>
2016-12-28 21:30:40 +0000

    Day 3 part 2

--- lib/day3/day3.ex
+++ lib/day3/day3.ex
@@ -1,11 +1,27 @@
 defmodule AdventOfCode.Day3 do
 
-  def start do
-    {:ok, contents} = File.read("../day3/input.txt")
-
-    contents
+  def input do
+    "../day3/input.txt"
+    |> File.read!
     |> String.split("\n", trim: true)
     |> Enum.map(&cleanup/1)
+  end
+
+  def part1 do
+    input
+    |> Enum.filter(&is_valid_triangle?/1)
+    |> Enum.count
+  end
+
+  def part2 do
+    sides = input |> List.flatten
+
+    a = sides |> Enum.take_every(3)
+    b = sides |> Enum.drop(1) |> Enum.take_every(3)
+    c = sides |> Enum.drop(2) |> Enum.take_every(3)
+
+    a ++ b ++ c
+    |> Enum.chunk(3)
     |> Enum.filter(&is_valid_triangle?/1)
     |> Enum.count
   end
