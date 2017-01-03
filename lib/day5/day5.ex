b68fef96930a77c39ae9db29de448c2517d7c47a
Tomasz Tomczyk <me@tomasztomczyk.com>
2017-01-03 23:10:27 +0000

    Day 5, part 2

--- lib/day5/day5.ex
+++ lib/day5/day5.ex
@@ -19,6 +19,50 @@ defmodule AdventOfCode.Day5 do
     :os.system_time(:seconds) - start
   end
 
+  def part2 do
+    start = :os.system_time(:seconds)
+
+    Stream.resource(
+      fn -> 1 end,
+      fn(index) ->
+        {[get_character_position(@input, index)], index + 1}
+      end,
+      fn(num) -> num end
+    )
+    |> Stream.filter(fn(val) -> !is_nil(val) end)
+    |> Enum.reduce_while(%{}, fn({position, char}, password) ->
+      new_password = create_password(position, char, password)
+
+      cond do
+        Map.keys(new_password) |> length < 8 ->
+          {:cont, new_password}
+        true -> {:halt, new_password}
+      end
+    end)
+    |> Map.values
+    |> List.to_string
+    |> IO.inspect
+
+    :os.system_time(:seconds) - start
+  end
+
+  def create_password(position, char, password) when is_integer(position) and position < 8, do: Map.put_new(password, position, char)
+  def create_password(position, char, password), do: password
+
+  def get_character_position(id, index, offset \\ 5) do
+    hash = hash(id, index)
+    case Regex.match?(~r/^00000/, hash) do
+      true ->
+        rest = hash |> String.split("") |> Enum.drop(offset)
+
+        case Enum.at(rest, 0) |> Integer.parse do
+          :error -> nil
+          {pos, _} -> {pos, Enum.at(rest, 1)}
+        end
+      _  -> nil
+    end
+  end
+
   def get_character(id, index, offset \\ 5) do
     hash = hash(id, index)
     case Regex.match?(~r/^00000/, hash) do
