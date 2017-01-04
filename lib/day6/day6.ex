2b975ebea04981e0fc8427d3490f50052b581e6c
Tomasz Tomczyk <me@tomasztomczyk.com>
2017-01-04 23:16:11 +0000

    DRYing up day 6

--- lib/day6/day6.ex
+++ lib/day6/day6.ex
@@ -1,6 +1,6 @@
 defmodule AdventOfCode.Day6 do
 
-  def part1 do
+  def input do
     "../day6/input.txt"
     |> File.stream!
     |> Stream.map(&String.strip/1)
@@ -12,28 +12,24 @@ defmodule AdventOfCode.Day6 do
       |> Enum.reduce(%{}, fn(char, acc) ->
         Map.update(acc, char, 1, &(&1 + 1))
       end)
-      |> Enum.max_by(fn({_, count}) -> count end)
-      |> Tuple.to_list
-      |> List.first
+      |> Enum.sort_by(fn({_, count}) -> count end)
+    end)
+  end
+
+  def part1 do
+    input
+    |> Enum.map(fn(chars) ->
+      chars
+      |> List.last |> Tuple.to_list |> List.first
     end)
     |> Enum.join("")
   end
 
   def part2 do
-    "../day6/input.txt"
-    |> File.stream!
-    |> Stream.map(&String.strip/1)
-    |> Enum.map(&String.split(&1, "", trim: true))
-    |> List.zip
+    input
     |> Enum.map(fn(chars) ->
       chars
-      |> Tuple.to_list
-      |> Enum.reduce(%{}, fn(char, acc) ->
-        Map.update(acc, char, 1, &(&1 + 1))
-      end)
-      |> Enum.min_by(fn({_, count}) -> count end)
-      |> Tuple.to_list
-      |> List.first
+      |> List.first |> Tuple.to_list |> List.first
     end)
     |> Enum.join("")
   end
