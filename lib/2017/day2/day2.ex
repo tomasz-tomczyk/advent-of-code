8c51b9b686ae2b4f10449069bd28c7a5d7386626
Tomasz Tomczyk <me@tomasztomczyk.com>
2017-12-04 09:34:29 +0000

    2017 - Day 2 solution

---
+++ lib/2017/day2/day2.ex
@@ -0,0 +1,25 @@
+defmodule AdventOfCode.TwentySeventeen.Day2 do
+
+
+  def input do
+    "lib/2017/day2/input.txt"
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn(s) -> s |> String.split |> Enum.map(&String.to_integer/1) end)
+  end
+
+  def part1 do
+    input
+    |> Enum.map(fn(row) -> Enum.max(row) - Enum.min(row) end)
+    |> Enum.sum
+  end
+
+  def part2 do
+    input
+    |> Enum.map(fn(row) ->
+      out = for x <- row, y <- row, x != y, rem(x, y) == 0, do: div(x, y)
+      hd(out)
+    end)
+    |> Enum.sum
+  end
+end
