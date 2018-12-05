5538ebfdaaa528ce006718d997de614b15d24912
Tomasz Tomczyk <me@tomasztomczyk.com>
2018-12-05 20:46:51 +0000

    2018 - Day 2 - part 2

--- lib/2018/day1/day1.ex
+++ lib/2018/day1/day1.ex
@@ -4,6 +4,29 @@ defmodule AdventOfCode.Year2018.Day1 do
     |> Enum.reduce(0, &process/2)
   end
 
+  def part2 do
+    {0, MapSet.new([0])}
+    |> loop(input())
+  end
+
+  def loop({sum, frequencies_seen}, input) do
+    input
+    |> Enum.reduce_while({sum, frequencies_seen}, fn current_frequency, {sum, frequencies_seen} ->
+      new_frequency = process(current_frequency, sum)
+
+      case MapSet.member?(frequencies_seen, new_frequency) do
+        true ->
+          {:halt, new_frequency}
+
+        false ->
+          {:cont, {new_frequency, MapSet.put(frequencies_seen, new_frequency)}}
+      end
+    end)
+    |> loop(input)
+  end
+
+  def loop(sum, _input), do: sum
+
   def process("+" <> frequency, sum), do: sum + String.to_integer(frequency)
   def process("-" <> frequency, sum), do: sum - String.to_integer(frequency)
 
--- test/2018/day1_test.exs
+++ test/2018/day1_test.exs
@@ -4,4 +4,8 @@ defmodule AdventOfCode.Year2018.Day1Test do
   test "Day 1 part 1 returns correct number" do
     assert AdventOfCode.Year2018.Day1.part1() == 569
   end
+
+  test "Day 1 part 2 returns correct number" do
+    assert AdventOfCode.Year2018.Day1.part2() == 77666
+  end
 end
